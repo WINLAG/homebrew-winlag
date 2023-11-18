373c152f1aa388ebded0813df19f2e51f49d5a1d
WINLAG <150890700+WINLAG@users.noreply.github.com>
2023-11-18 17:11:36 +0200

    0.3 public preview

--- winlag.rb
+++ winlag.rb
@@ -2,9 +2,9 @@ class Winlag < Formula
     desc "WINLAG"
     homepage "https://www.winlag.com/"
     url "https://www.winlag.com/download/macos" 
-    sha256 "72ea7adb7a7f025aa6dd0587de039aa6e489d4d2a133e3a849ae178fd843aa85"
+    sha256 "3809d13dc4aa8f79e8c428766f27352ab999a43b89e11bdd5657ca2498054350"
   
-    version "0.2"
+    version "0.3"
   
     depends_on "openssl@3"
     depends_on "cryptopp"
@@ -14,7 +14,7 @@ class Winlag < Formula
 
     def install
         bin.install "winlag"
-        bin.install_symlink "#{bin}/winlag" => "winlag-02PP"
+        bin.install_symlink "#{bin}/winlag" => "winlag-03PP"
         bin.install "fonts"
         bin.install "workspaces"
         bin.install "data"
